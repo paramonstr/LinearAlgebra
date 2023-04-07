519fae0ecd5bc189fda7eb2dca11cb04f8d18847
laura <boni3.c.g@gmail.com>
2023-04-07 22:29:20 +0200

    make corrections in naming of variables

--- LinearTransformation.py
+++ LinearTransformation.py
@@ -4,6 +4,10 @@ import matplotlib.patches as patches
 
 
 def matrix_to_xy_coordinates(matrix):
+    """
+    :param matrix: 2 * 2 matrix
+    :return: corner-coordinates of the span of the matrix
+    """
     origin = np.array([0, 0])
     ex, ey = matrix_to_vectors(matrix)
     corner = ex + ey
@@ -34,8 +38,8 @@ def matrix_to_vectors(matrix):
 
 def linear_transformation(transformation_matrix, v):
     """
-    :param transformation_matrix: matrix of transformed unit vector
-    :param v: vector v to the normal basis unit vectors
+    :param transformation_matrix: 2 * 2 transformation matrix
+    :param v: original vector to the standard base ex, ey
     :return: transformed vector v to the transformation matrix
     """
     transformed_v = np.matmul(transformation_matrix, np.vstack(v))
@@ -45,12 +49,22 @@ def linear_transformation(transformation_matrix, v):
 def plot_2d_transformation(original_vector, transformation_matrix, draw_span=True):
     """
     :param original_vector: original vector to the standard base ex, ey
-    :param transformation_matrix: matrix of transformed unit vector
+    :param transformation_matrix: matrix of transformed unit vectors
     :param draw_span: default true, will draw the span (determinant) of the original and transformed base
-    :return:
+    :return: plot
     """
+
+    # original vector
+    ex = np.array([1, 0])
+    ey = np.array([0, 1])
+    original_base = vectors_to_matrix(ex, ey)
+
+    # transformed vector
+    v_trans = linear_transformation(transformation_matrix, original_vector)
+    ex_trans, ey_trans = matrix_to_vectors(transformation_matrix)
+
     fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(10, 5))
-    system_size = transformation_matrix.max() + 3
+    system_size = abs(v_trans).max()
     xmin, xmax, ymin, ymax = -system_size, system_size, -system_size, system_size
     ticks_frequency = 1
     x_ticks = np.arange(xmin, xmax + 1, ticks_frequency)
@@ -65,7 +79,7 @@ def plot_2d_transformation(original_vector, transformation_matrix, draw_span=Tru
                yticks=y_ticks[y_ticks != 0]  # draw ticks and remove 0 label on y axis
                )
 
-        for spine in ["bottom", "left"]:  # draw coordinate center at x = 0 and y = 0 x axis y = 0
+        for spine in ["bottom", "left"]:  # draw coordinate center at x = 0 and y = 0
             ax.spines[spine].set_position('zero')
 
         for spine in ["top", "right"]:  # remove plot borders
@@ -75,10 +89,7 @@ def plot_2d_transformation(original_vector, transformation_matrix, draw_span=Tru
         ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
         ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
 
-    # draw to the original basis and vector
-    ex = np.array([1, 0])
-    ey = np.array([0, 1])
-    original_base = vectors_to_matrix(ex, ey)
+    # draw the original vector
     original_vectors = [ex, ey, original_vector]
     for vector in original_vectors:
         if np.array_equal(vector, original_vector):
@@ -87,9 +98,7 @@ def plot_2d_transformation(original_vector, transformation_matrix, draw_span=Tru
             color = "gray"
         axes[0].quiver(0, 0, vector[0], vector[1], color=color, angles='xy', scale_units='xy', scale=1)
 
-    # draw to the transformed basis and vector
-    v_trans = linear_transformation(transformation_matrix, original_vector)
-    ex_trans, ey_trans = matrix_to_vectors(transformation_matrix)
+    # draw vector to the transformation matrix
     transformed_vectors = [ex_trans, ey_trans, v_trans]
     for vector in transformed_vectors:
         if np.array_equal(vector, v_trans):
@@ -98,15 +107,15 @@ def plot_2d_transformation(original_vector, transformation_matrix, draw_span=Tru
             color = "gray"
         axes[1].quiver(0, 0, vector[0], vector[1], color=color, angles='xy', scale_units='xy', scale=1)
 
-    # draw span/determinant of original and transformed basis
+    # draw span/determinant of original and transformed
     if draw_span:
         axes[0].add_patch(patches.Polygon(xy=matrix_to_xy_coordinates(original_base), color="green", alpha=0.1))
         axes[1].add_patch(patches.Polygon(xy=matrix_to_xy_coordinates(transformation_matrix), color="blue", alpha=0.1))
 
     # titles
-    axes[0].set_title(f"original to ex = {ex} and ex = {ey}",
+    axes[0].set_title(f"original to ex = {ex} and ey = {ey}",
                       y=1.08)  # y = 1.08 elevates the title from the plot by 8 %
-    axes[1].set_title(f"transformed on ex = {ex_trans} and ex = {ey_trans}",
+    axes[1].set_title(f"transformed on ex = {ex_trans} and ey = {ey_trans}",
                       y=1.08)  # y = 1.08 elevates the title from the plot by 8 %
 
     fig.tight_layout()
@@ -114,6 +123,6 @@ def plot_2d_transformation(original_vector, transformation_matrix, draw_span=Tru
 
 
 if __name__ == "__main__":
-    v = np.array([10, 2])
-    transformation_matrix = vectors_to_matrix(np.array([-3, 1]), np.array([-4, 0]))
-    plot_2d_transformation(v, transformation_matrix).show()
+    v = np.array([2, 3])
+    transform = vectors_to_matrix(np.array([5, -2]), np.array([-3, -1]))
+    plot_2d_transformation(v, transform).show()
